504df774fefcdca8346d4ec40b5cf3479654abb1
Eros Cedeño <eycer1@gmail.com>
2022-12-19 19:54:11 -0400

    Probando 2c

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 -- PREGUNTA 2a
 -- Se ejecuta la funcion
--- shortest_path( origin_city, steps, table_name_out ) sobre
+-- shortest_path( origin_city, table_name_out, steps ) sobre
 -- cada ciudad en la lista de ciudades con maximo una parada (step = 1)
 
 SELECT shortest_path( c.id, 'pregunta_2a', 1 )
@@ -16,7 +16,7 @@ JOIN city as f ON f.id = p1.to_city;
 
 -- PREGUNTA 2b
 -- Se ejecuta la funcion
--- shortest_path( origin_city, steps, table_name_out ) sobre
+-- shortest_path( origin_city, table_name_out, steps ) sobre
 -- cada ciudad en la lista de ciudades con maximo dos paradas (step = 2)
 
 SELECT shortest_path( c.id, 'pregunta_2b', 2 )
@@ -27,4 +27,19 @@ SELECT o.name as origin, f.name as destination, p1.price as min_cost, p1.steps, 
 FROM pregunta_2b as p1
 JOIN city as o ON o.id = p1.from_city
 JOIN city as f ON f.id = p1.to_city
+
+-- PREGUNTA 2c
+-- Se ejecuta la funcion
+-- shortest_path( origin_city, table_name_out, steps ) sobre
+-- cada ciudad en la lista de ciudades sin paradas maximas
+
+SELECT shortest_path( c.id, 'pregunta_2b')
+FROM city as c;
+
+-- MOSTRAMOS EL RESULTADO
+SELECT o.name as origin, f.name as destination, p1.price as min_cost, p1.steps, p1.visited as path
+FROM pregunta_2b as p1
+JOIN city as o ON o.id = p1.from_city
+JOIN city as f ON f.id = p1.to_city
+
 
