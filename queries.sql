8c92696ff46f6004b32d571703f4ae9bc7cfad1b
Eros Cedeño <eycer1@gmail.com>
2022-12-19 19:55:06 -0400

    Probando 2c

--- queries.sql
+++ queries.sql
@@ -26,7 +26,7 @@ FROM city as c;
 SELECT o.name as origin, f.name as destination, p1.price as min_cost, p1.steps, p1.visited as path
 FROM pregunta_2b as p1
 JOIN city as o ON o.id = p1.from_city
-JOIN city as f ON f.id = p1.to_city
+JOIN city as f ON f.id = p1.to_city;
 
 -- PREGUNTA 2c
 -- Se ejecuta la funcion
@@ -40,6 +40,6 @@ FROM city as c;
 SELECT o.name as origin, f.name as destination, p1.price as min_cost, p1.steps, p1.visited as path
 FROM pregunta_2b as p1
 JOIN city as o ON o.id = p1.from_city
-JOIN city as f ON f.id = p1.to_city
+JOIN city as f ON f.id = p1.to_city;
 
 
